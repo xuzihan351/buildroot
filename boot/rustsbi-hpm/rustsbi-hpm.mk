278696fc76a03ffc5243ca7249b51b228734be10
tfx2001 <tfx2001@outlook.com>
2024-07-27 23:56:05 +0800

    boot/rustsbi-hpm: new package
    Signed-off-by: tfx2001 <tfx2001@outlook.com>

---
+++ boot/rustsbi-hpm/rustsbi-hpm.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# rustsbi-hpm
+#
+################################################################################
+
+RUSTSBI_HPM_VERSION = d2ffb6d76a59f2aaf8a64de1602164cffd62b77a
+RUSTSBI_HPM_SITE = $(call github,hpm-rs,rustsbi-hpm,$(RUSTSBI_HPM_VERSION))
+
+RUSTSBI_HPM_INSTALL_IMAGES = YES
+
+define RUSTSBI_HPM_BUILD_CMDS
+	cd $(RUSTSBI_HPM_SRCDIR) && \
+	cargo build --features flash --release && \
+	cargo objcopy --features flash --release -- -O binary $(@D)/rustsbi.bin
+endef
+
+define RUSTSBI_HPM_INSTALL_IMAGES_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/rustsbi.bin $(BINARIES_DIR)/rustsbi.bin
+endef
+
+$(eval $(generic-package))
