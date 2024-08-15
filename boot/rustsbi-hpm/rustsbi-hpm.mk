d0fc7eaa10249685ef52186816fc388a0efcc228
tfx2001 <tfx2001@outlook.com>
2024-08-16 02:23:33 +0800

    boot/rustsbi-hpm: update to 0cf5ef0
    In this revision, rustsbi-hpm support emulate RVA ISA with SDRAM address
    range that we can run Linux kernel without any modification and enable
    pthread support.
    
    Signed-off-by: tfx2001 <tfx2001@outlook.com>

--- boot/rustsbi-hpm/rustsbi-hpm.mk
+++ boot/rustsbi-hpm/rustsbi-hpm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RUSTSBI_HPM_VERSION = 4637e866fd38882dca4a17790fdffe1c9e687b8d
+RUSTSBI_HPM_VERSION = 0cf5ef0022bbe2c4a5f98d797bf6d3837b7eefa1
 RUSTSBI_HPM_SITE = $(call github,rustsbi,rustsbi-hpm,$(RUSTSBI_HPM_VERSION))
 
 RUSTSBI_HPM_INSTALL_IMAGES = YES
